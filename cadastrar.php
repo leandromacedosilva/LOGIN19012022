c1f948c0b0686c0f784b26af6f9f69ef4e0002d4
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-01-19 07:03:54 -0300

    added file cadastrar.php in github

---
+++ cadastrar.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Login Project</title>
+</head>
+<body>
+    <div id="corpo-form-cad">
+    <h1> <strong> TELA DE CADASTRO </strong> </h1>
+    <form action="process.php" method="POST">
+        <input type="text" placeholder="Nome completo...">
+        <input type="text" placeholder="Telefone (xx)xxxxxxxxx">
+        <input type="email" placeholder="Usuário">
+        <input type="password" placeholder="Senha">
+        <input type="password" placeholder="Confirmar senha...">
+        <input type="submit" value="Cadastrar">
+    </form>
+    </div>
+</body>
+</html>

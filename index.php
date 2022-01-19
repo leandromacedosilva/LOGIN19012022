c4e9de5389704e145bccb45cbadc7eb2e0f631a3
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-01-19 07:04:11 -0300

    added file index.php in github

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Login Project</title>
+</head>
+<body>
+    <div id="corpo-form">
+    <h1> <strong> ENTRAR </strong> </h1>
+    <form action="process.php" method="POST">
+        <input type="email" placeholder="Usuário">
+        <input type="password" placeholder="Senha">
+        <input type="submit" value="Acessar">
+        <a href="cadastrar.php">Não tem acesso ainda? <strong>Cadastre-se!</strong> </a>
+    </form>
+    </div>
+</body>
+</html>
